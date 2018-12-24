90dbf0375b589e5e589b2e4251abb9bf12729a4a
ching <qinshiwo>
2018-12-24 20:08:43 +0800

    Initial commit

---
+++ src/term_1/ConcurrencyTest.java
@@ -0,0 +1,62 @@
+package term_1;
+/*
+ *
+ *     @author Qmh
+ *
+ *   Yuan Li Bug Zhen Ai Sheng Ming
+ */
+
+
+import java.util.Objects;
+
+public class ConcurrencyTest {
+
+    /**
+     * 这是一个测试串行和并发的速度例子
+     */
+
+
+    private static final long count = 100001;
+
+    public static void main(String[] args) throws InterruptedException {
+        concurrency();
+        serial();
+    }
+
+    private static void concurrency() throws InterruptedException {
+
+        long start = System.currentTimeMillis();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int a = 0;
+                for (long i = 0; i < count; i++) {
+                    a += 5;
+                }
+            }
+        });
+        thread.start();
+        int b = 0;
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        thread.join();
+        long time = System.currentTimeMillis() - start;
+        System.out.println("concurrency : " + time + "ms , b=" + b );
+    }
+
+    private static void serial() {
+        long start = System.currentTimeMillis();
+        int a = 0;
+        for (long i = 0; i < count; i++) {
+            a += 5;
+        }
+        int b = 0;
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        long time = System.currentTimeMillis() - start;
+        System.out.println("serial : " + time + "ms , b=" + b + ", a=" + a);
+    }
+
+}
